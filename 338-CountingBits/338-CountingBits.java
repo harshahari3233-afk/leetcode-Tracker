b2ea1915da1fb72581405a40419daa5a6730ded0
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:08:42 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 3:08:43 PM
+class Solution {
+    public int[] countBits(int n) {
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            dp[i]=dp[(i & (i-1))]+1;
+        }
+        return dp;
+       
+    }
+    
+
+}
