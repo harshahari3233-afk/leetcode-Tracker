feb34fa644dd774538262f2bf02b44bda35852a3
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:06:52 +0530

    Create 1630-CountOddNumbersInAnIntervalRange

---
+++ 1630-CountOddNumbersInAnIntervalRange/1630-CountOddNumbersInAnIntervalRange.java
@@ -0,0 +1,12 @@
+// Last updated: 7/9/2026, 3:06:53 PM
+class Solution {
+    public int countOdds(int low, int high) {
+        if(low%2==0){
+            low++;
+        }
+        if(low>high){
+            return 0;
+        }
+        return (high-low)/2+1;
+    }
+}
