23980249129f6b94f7ee99e20f46f931d878c95d
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:11:29 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 3:11:30 PM
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i]+=1;
+                return digits;
+            }
+            digits[i]=0;
+        }
+       int[] result=new int[n+1];
+       result[0]=1;
+       return result; 
+    }
+}
