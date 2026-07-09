bafd26c5f8bddaa129ab7b03e1aa5e223ddd94d9
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:06:45 +0530

    Create 1801-AverageTimeOfProcessPerMachine

---
+++ 1801-AverageTimeOfProcessPerMachine/1801-AverageTimeOfProcessPerMachine.sql
@@ -0,0 +1,11 @@
+-- Last updated: 7/9/2026, 3:06:46 PM
+# Write your MySQL query statement below
+SELECT a.machine_id,
+       ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a
+JOIN Activity b
+ON a.machine_id = b.machine_id
+AND a.process_id = b.process_id
+WHERE a.activity_type = 'start'
+AND b.activity_type = 'end'
+GROUP BY a.machine_id;
