8abbbf2c8f09cb8554e2192965769c08d8044439
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:08:06 +0530

    Create 543-DiameterOfBinaryTree

---
+++ 543-DiameterOfBinaryTree/543-DiameterOfBinaryTree.java
@@ -0,0 +1,32 @@
+// Last updated: 7/9/2026, 3:08:07 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int d=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+       height(root);
+       return d;
+    }
+    public int height(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int left=height(root.left);
+        int right=height(root.right);
+         d=Math.max(d,right+left);
+        return 1+Math.max(left,right);
+    }
+}
