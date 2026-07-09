73b7261dc93e8445dc2c6a5ff071abfe1a35a924
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:08:38 +0530

    Create 367-ValidPerfectSquare

---
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.java
@@ -0,0 +1,26 @@
+// Last updated: 7/9/2026, 3:08:39 PM
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long i=1;
+        long j=num-1;
+        long mid=i+(j-i)/2;
+        if(num==1){
+            return true;
+        }
+        while(i<=j){
+            if(mid*mid==num){
+                return true;
+            }
+            else if(mid*mid<num){
+                i=mid+1;
+            }
+            else if(mid*mid>num){
+                j=mid-1;
+            }
+            mid=i+(j-i)/2;
+
+        }
+        return false;
+
+    }
+}
