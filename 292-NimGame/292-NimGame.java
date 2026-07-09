f94c3eacece9bc7a154b255bc520ca37d5aab019
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:08:55 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,9 @@
+// Last updated: 7/9/2026, 3:08:56 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4!=0){
+            return true;
+        }
+        return false;
+    }
+}
