870308d13d5dc13b67be78f898cbb252e4303d0d
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:05:50 +0530

    Create 2679-CountDistinctNumbersOnBoard

---
+++ 2679-CountDistinctNumbersOnBoard/2679-CountDistinctNumbersOnBoard.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 3:05:51 PM
+class Solution {
+    public int distinctIntegers(int n) {
+        if(n==1){
+            return 1;
+        }
+        else{
+            return n-1;
+        }
+    }
+}
