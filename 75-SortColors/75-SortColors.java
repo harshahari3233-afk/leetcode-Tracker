59968420594023ed7ec158ecd721972100cb65c8
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:11:23 +0530

    Create 75-SortColors

---
+++ 75-SortColors/75-SortColors.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 3:11:23 PM
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+            if(nums[i]>=nums[j]){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+            }
+        }
+    }
+}
