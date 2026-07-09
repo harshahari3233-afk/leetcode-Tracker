830ab6082c26dc1c617a984de4d38dbb908e253e
harshahari3233-afk <harshahari3233@gmail.com>
2026-07-09 15:07:28 +0530

    Create 969-NumberOfRecentCalls

---
+++ 969-NumberOfRecentCalls/969-NumberOfRecentCalls.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 3:07:28 PM
+class RecentCounter {
+    Queue<Integer> q;
+    public RecentCounter() {
+        q=new LinkedList<>();
+    }
+    
+    public int ping(int t) {
+        q.offer(t);
+        while(!q.isEmpty() && q.peek()<t-3000){
+            q.poll();
+        }
+        return q.size();
+    }
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
